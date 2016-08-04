741665660a7db198af1cfe57433cdd9b6d839586
IvanRzhanoi <Ivan.Rzhanoi@gmail.com>
2016-08-04 17:09:06 +0900

    Initial Commit

---
+++ gitDemoIR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoIR
+//
+//  Created by Ivan Rzhanoi on 04/08/16.
+//  Copyright © 2016 Ivan Rzhanoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
